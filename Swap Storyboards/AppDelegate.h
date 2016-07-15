53b56e0c1aa66f5108486fd18721ea9a4941299e
Jay Versluis <jay@versluis.com>
2016-07-15 10:05:41 -0400

    Initial Commit

---
+++ Swap Storyboards/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Swap Storyboards
+//
+//  Created by Jay Versluis on 15/07/2016.
+//  Copyright © 2016 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
