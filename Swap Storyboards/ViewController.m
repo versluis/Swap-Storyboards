32fd6c9562c9eaf415b5a8b9d8615296789c1eb2
Jay Versluis <jay@versluis.com>
2016-07-15 10:35:36 -0400

    load storyboard during transition

--- Swap Storyboards/ViewController.m
+++ Swap Storyboards/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AppDelegate.h"
 
 @interface ViewController ()
 
@@ -22,6 +23,66 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+# pragma mark -
+
+- (UIStoryboard *)grabPortraitStoryboard {
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Portrait" bundle:nil];
+    return storyboard;
+}
+
+- (UIStoryboard *)grabLandscapeStoryboard {
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Landscape" bundle:nil];
+    return storyboard;
+}
+
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id)coordinator {
+    
+    // best call super just in case
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    
+    // will execute before rotation
+    [coordinator animateAlongsideTransition:^(id  _Nonnull context) {
+        
+        // will execute during rotation
+    } completion:^(id  _Nonnull context) {
+        
+        // setup some references
+        UIStoryboard *storyboard;
+        AppDelegate *myAppDelegate = [UIApplication sharedApplication].delegate;
+        
+        // are we in portrait mode?
+        if (size.height == 1024) {
+            NSLog(@"We're in portrait mode");
+            
+            // grab portrait storyboard
+            storyboard = [self grabPortraitStoryboard];
+            
+            // and display it
+            myAppDelegate.window.rootViewController = [storyboard instantiateInitialViewController];
+            [myAppDelegate.window makeKeyAndVisible];
+
+        }
+        
+        // are we in landscape mode?
+        if (size.height == 768) {
+            NSLog(@"We're in landscape mode.");
+            
+            // grab landscape storyboard
+            storyboard = [self grabLandscapeStoryboard];
+            
+            // and display it
+            myAppDelegate.window.rootViewController = [storyboard instantiateInitialViewController];
+            [myAppDelegate.window makeKeyAndVisible];
+        }
+        
+        // will execute after rotation
+        
+    }];
 }
 
 @end
